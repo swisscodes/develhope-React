9e83fbb948364cd80b0ba0564350a660ead2c1cc
Obafemi Lawal <swissbobo@gmail.com>
2022-11-17 11:36:51 +0300

    ex 05 done

--- develhope/src/App.js
+++ develhope/src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 
 function App() {
   var hello = <h1>Hello</h1>;
-  return <div className='App'>{hello}</div>;
+  return (
+    <div className='App'>
+      <div>{hello}</div>
+      {returnsH1('Obafemi')}
+      {aAndB(5, 10)}
+    </div>
+  );
+}
+
+//functions
+
+/* JSX - 02 */
+function returnsH1(name) {
+  return <h1>Hello {name}</h1>;
+}
+
+function aAndB(a, b) {
+  return <h2>{a + b}</h2>;
 }
 
 export default App;
---
+++ develhope/src/components/returnsH1.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function returnsH1({ name }) {
+  return <h1>Hello {name}</h1>;
+}
+
+export default returnsH1;
