326e6ad9fbaf3fa358e473b5dc210290517f53a0
Obafemi Lawal <swissbobo@gmail.com>
2022-11-17 12:56:11 +0300

    comp 01|

--- components/Hello.jsx
+++ components/Hello.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 class Hello extends React.Component {
   render() {
-    return <h1>Hello, World!</h1>;
+    return (
+      <>
+        <h1>Hello, World!</h1>
+        <p>What a beautiful day!</p>
+      </>
+    );
   }
 }
 export default Hello;
