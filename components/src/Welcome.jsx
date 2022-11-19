ad36bab979364f9545a0cb9c2fe90058beebb89d
Obafemi Lawal <swissbobo@gmail.com>
2022-11-19 13:48:05 +0300

    comp 01|

--- components/src/Welcome.jsx
+++ components/src/Welcome.jsx
@@ -7,6 +7,17 @@ class Welcome extends React.Component {
     return (
       <>
         <p>Welcome {name}</p>
+
+        {/*Conditional Rendering - 01 */}
+        {age > 18 && <Age age={age} />}
+
+        {/*Conditional Rendering - 02 */}
+        {age && <Age age={age} />}
+
+        {/*Conditional Rendering - 03 */}
+        {age > 18 && age < 65 && <Age age={age} />}
+
+        {/*Conditional Rendering - 03 */}
         {age > 18 && age < 65 && name === 'john' && <Age age={age} />}
       </>
     );
