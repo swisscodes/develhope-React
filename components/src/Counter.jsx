a16604e8c08e3f1478d31554523cabdb2c94a17f
Obafemi Lawal <swissbobo@gmail.com>
2022-11-19 15:48:44 +0300

    comp 01|

--- components/src/Counter.jsx
+++ components/src/Counter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CounterDisplay from './CounterDisplay';
 
 class Counter extends React.Component {
   state = {
@@ -16,10 +17,40 @@ class Counter extends React.Component {
   render() {
     return (
       <>
-        <h1>{this.state.count}</h1>
+        <CounterDisplay count={this.state.count} />
       </>
     );
   }
 }
 
 export default Counter;
+
+/* 
+STATE 01
+    componentDidMount() {
+    setInterval(() => {
+      this.setState((state) => (state.count += 1));
+    }, 1000);
+  }
+    return (
+        <>
+            <h1>{this.state.count} </h1>
+        </>
+        );
+
+
+  STATE 03
+    componentDidMount() {
+        const { initValue, incrementBy } = this.props;
+        this.setState((state) => (state.count = initValue));
+        setInterval(() => {
+        this.setState((state) => (state.count += incrementBy));
+        }, 1000);
+    }
+
+    return (
+      <>
+        <h1>{this.state.count} </h1>
+      </>
+    );
+*/
---
+++ components/src/CounterDisplay.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CounterDisplay({ count }) {
+  return <h1>{count}</h1>;
+}
+
+export default CounterDisplay;
