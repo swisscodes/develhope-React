590c4e978a567c2eef84dd874ed554ca61418c29
Obafemi Lawal <swissbobo@gmail.com>
2022-11-20 16:01:47 +0300

    comp 01|

--- components/src/Login.jsx
+++ components/src/Login.jsx
@@ -22,6 +22,7 @@ function Login() {
       <input
         type='checkbox'
         name='remember'
+        checked={formFields.remember ?? false}
         onChange={(e) => getFormFields(e)}
       />
       <button
