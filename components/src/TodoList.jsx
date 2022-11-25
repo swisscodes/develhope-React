b6f5abdfd311288275dd594b0dfad8e192c894c6
Obafemi Lawal <swissbobo@gmail.com>
2022-11-25 10:55:31 +0300

    OK

--- components/src/TodoList.jsx
+++ components/src/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function TodoList() {
   const [items, setItem] = useState([]);
-  const [typedValue, setTypeValue] = useState();
+  const [typedValue, setTypeValue] = useState('');
   return (
     <div>
       <ul>
@@ -10,15 +10,23 @@ function TodoList() {
           <li key={item + i}>{item}</li>
         ))}
       </ul>
-      <input type='text' onChange={(el) => onType(el)} />
-      <button type='button' onClick={() => onAdd()}>
+      <input type='text' value={typedValue} onChange={(el) => onType(el)} />
+      <button type='button' onClick={(el) => onAddOrReset(el)}>
         Add
+      </button>
+      <button type='reset' onClick={(el) => onAddOrReset(el)}>
+        Rest
       </button>
     </div>
   );
 
-  function onAdd(el) {
+  function onAddOrReset(el) {
+    if (el.target.type === 'reset') {
+      setItem(() => []);
+      return;
+    }
     setItem(() => [...items, typedValue]);
+    setTypeValue(() => '');
   }
   function onType(el) {
     setTypeValue(() => el.target.value);
