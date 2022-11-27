b6dabfb1cfc7fe92dccd6c7b4854f77276688612
Obafemi Lawal <swissbobo@gmail.com>
2022-11-27 10:33:59 +0300

    OK

--- components/src/Sum.jsx
+++ components/src/Sum.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Sum({ numbers }) {
+function Sum({ numbers = [1, 2, 4, 5] }) {
   return (
     <div>
       <h1>{numbers.reduce((acc, val) => acc + val, 0)}</h1>
