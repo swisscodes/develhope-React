93e484c3d8226df40d415ec6d0a3243a5b37a4de
Obafemi Lawal <swissbobo@gmail.com>
2022-11-30 15:57:44 +0300

    OK

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,56 +1,12 @@
 import React from 'react';
-import Hello from './Hello';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
 import Welcome from './Welcome';
-import Counter from './Counter';
-import ClickCounter from './ClickCounter';
-import ClickTracker from './ClickTracker';
-import InteractiveWelcome from './InteractiveWelcome';
-import Login from './Login';
-import UncontrolledLogin from './UncontrolledLogin';
-import TodoList from './TodoList';
-import Wrapped1 from './Wrapped1';
-import DisplayLanguage from './DisplayLanguage';
-import Sum from './Sum';
-import ClickCounterFunc from './ClickCounterFunc';
-import CounterFunc from './CounterFunc';
-import Github from './Github';
-import GithubUsers from './GithubUsers';
-import Githubuser1 from './Githubuser1';
-import CardDetails from './CardDetails';
-import FilteredList from './FilteredList';
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <Hello />
-        <Welcome name=<strong>{'Obafemi'}</strong> age={35} />
-        <Counter initValue={1} incrementBy={1} />
-        <ClickCounter initValue={1} incrementBy={1} />
-        <ClickTracker />
-        <InteractiveWelcome />
-        <Login />
-        <UncontrolledLogin />
-        <TodoList />
-        <Wrapped1 />
-        <DisplayLanguage />
-        <Sum numbers={[1, 2, 3, 4, 5]} />
-        <ClickCounterFunc initValue={1} incrementBy={1} />
-        <CounterFunc />
-        <Github />
-        <GithubUsers />
-        <Githubuser1 />
-        <CardDetails />
-        <FilteredList
-          todosList={[
-            { name: 'Obafemi', age: 35, id: 1 },
-            { name: 'guy', age: 16, id: 2 },
-            { name: 'babe', age: 25, id: 3 },
-            { name: 'andy', age: 56, id: 4 },
-          ]}
-        />
-      </div>
-    );
+    return <div></div>;
   }
 }
 
--- components/src/index.jsx
+++ components/src/index.jsx
@@ -2,6 +2,16 @@ import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+import Welcome from './Welcome';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Welcome name='world' />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
